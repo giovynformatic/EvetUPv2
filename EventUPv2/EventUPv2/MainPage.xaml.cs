6113352a646a6854d0248d12c11c8aac12100e84
giovynformatic <giovynformatic@gmail.com>
2019-03-26 00:08:25 +0100

    verifica campi login

--- EventUPv2/EventUPv2/MainPage.xaml.cs
+++ EventUPv2/EventUPv2/MainPage.xaml.cs
@@ -11,25 +11,44 @@ namespace EventUPv2
         public MainPage()
         {
             InitializeComponent();
-      
+
         }
         async void OnRegisterClicked(object sender, EventArgs args)
         {
             await DisplayAlert("Alert", pass.Text, "OK");
             await Navigation.PushModalAsync(new Registrati());
-                
-                
-        }
-        async void OnLoginClicked(object sender, EventArgs args)
-        {
 
-            if (pass.Text=="peppo" && email.Text=="Pippo") {
+
+        }
+        void OnLoginClicked(object sender, EventArgs args)
+        {
+             Verify(email.Text, "Pippo", pass.Text, "peppo");
+
+
+        }
+
+        async void Verify(string a, string b, string c, string d)
+        {
+            if (a == b && c == d)
+            {
                 await Navigation.PushModalAsync(new HomePage());
             }
-            else {
+            else if (!string.IsNullOrEmpty(a) && a != b)
+            {
+                await DisplayAlert("Alert", "E-mail non Corretta!", "OK");
+            }
+            else if (string.IsNullOrEmpty(a))
+            {
+                await DisplayAlert("Alert", "E-mail non Inserita!", "OK");
+            }
+            else if (!string.IsNullOrEmpty(c) && c != d)
+            {
                 await DisplayAlert("Alert", "Password non Corretta!", "OK");
             }
-
+            else if (string.IsNullOrEmpty(c))
+            {
+                await DisplayAlert("Alert", "Password non Inserita!", "OK");
+            }
         }
 
     }
