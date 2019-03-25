f682ed072c39fc87ee7f2a95650f13cb3349428d
giovynformatic <giovynformatic@gmail.com>
2019-03-25 09:28:58 +0100

    ok

---
+++ EventUPv2/EventUPv2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace EventUPv2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+      
+        }
+        async void OnRegisterClicked(object sender, EventArgs args)
+        {
+            await DisplayAlert("Alert", pass.Text, "OK");
+            await Navigation.PushModalAsync(new Registrati());
+                
+                
+        }
+
+    }
+}
