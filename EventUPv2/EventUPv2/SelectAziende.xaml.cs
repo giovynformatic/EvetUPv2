5726d2ce0b761933b5006b8571befc0c42c0af5c
giovynformatic <giovynformatic@gmail.com>
2019-04-18 12:03:40 +0200

    select aziende

---
+++ EventUPv2/EventUPv2/SelectAziende.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EventUPv2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectAziende : ContentPage
+    {
+
+        public SelectAziende()
+        {
+            List<SelectableData<ExampleData>> SelectedData = new List<SelectableData<ExampleData>>()
+            {
+                new SelectableData<ExampleData>() { Data = new ExampleData() { Name = "Test1", Description = "Description1" } },
+                new SelectableData<ExampleData>() { Data = new ExampleData() { Name = "Test2", Description = "Description2" } },
+                new SelectableData<ExampleData>() { Data = new ExampleData() { Name = "Test3", Description = "Description3" } },
+                new SelectableData<ExampleData>() { Data = new ExampleData() { Name = "Test4", Description = "Description4" } },
+                new SelectableData<ExampleData>() { Data = new ExampleData() { Name = "Test5", Description = "Description5" } }
+            };
+
+
+            InitializeComponent();
+            BindingContext = new MultiSelectViewModel(SelectedData);
+        }
+    }
+}
