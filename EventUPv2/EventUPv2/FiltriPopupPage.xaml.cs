8473737f0eb561e08bcdd1b63430307382ced8f5
giovynformatic <giovynformatic@gmail.com>
2019-05-13 10:30:23 +0200

    filtripopup

---
+++ EventUPv2/EventUPv2/FiltriPopupPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Xamarin.Forms.Xaml;
+
+namespace EventUPv2
+{
+    public partial class FiltriPopupPage
+    {
+
+        public FiltriPopupPage()
+        {
+            InitializeComponent();
+            dataf.On = Constants.OrdineFiltri.ElementAt(0);
+            nomef.On = Constants.OrdineFiltri.ElementAt(1);
+            aziendaf.On = Constants.OrdineFiltri.ElementAt(2);
+
+
+        }
+
+        async void FinishOrdinamento(object sender, EventArgs args)
+        {
+
+            Boolean[] intersf = new Boolean[3] { dataf.On, nomef.On, aziendaf.On };
+
+            await Navigation.PopAsync();
+            Constants.CurrentUser.interessi = intersf;
+
+        }
+    }
+}
